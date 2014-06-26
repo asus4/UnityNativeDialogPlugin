d41ad537e563e9b803d16adefe172694b717bf48
shinriyo <shinriyo@gmail.com>
2014-06-27 00:18:47 +0900

    親切なことに、インデントとファイル名変更もコミットするよ
    親切なことに、インデントとファイル名変更もコミットするよ

---
+++ unity/NativeDialogPlugin/Assets/DialogTest.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+
+public class DialogTest : MonoBehaviour
+{
+    public string decideLabel;
+    public string cancelLabel;
+    public string closeLabel;
+    
+    void Start ()
+    {
+        DialogManager.Instance.SetLabel(decideLabel, cancelLabel, closeLabel);
+    }
+
+    void OnGUI ()
+    {
+        if (GUILayout.Button("aaa", GUILayout.MinWidth(200), GUILayout.MinHeight(100)))
+        {
+            DialogManager.Instance.ShowSelectDialog("aaa", (bool result) => {
+                Debug.Log("aaa"+result);
+            });
+        }
+
+        if (GUILayout.Button("bbb", GUILayout.MinWidth(200), GUILayout.MinHeight(100)))
+        {
+            DialogManager.Instance.ShowSelectDialog("b title" ,"bbb", (bool result) => {
+                Debug.Log("bbb"+result);
+            });
+        }
+
+        if (GUILayout.Button("ccc", GUILayout.MinWidth(200), GUILayout.MinHeight(100)))
+        {
+            DialogManager.Instance.ShowSubmitDialog("ccc", (bool result) => {
+                Debug.Log ("ccc");
+            });
+        }
+
+        if (GUILayout.Button("ddd", GUILayout.MinWidth(200), GUILayout.MinHeight(100)))
+        {
+            DialogManager.Instance.ShowSubmitDialog("d title", "ddd", (bool result) => {
+                Debug.Log ("ddd");
+            });
+        }
+
+        if(GUILayout.Button("eee auto dissmiss", GUILayout.MinWidth(200), GUILayout.MinHeight(100))) {
+            int id = DialogManager.Instance.ShowSelectDialog("eee", (bool result) => {
+                Debug.Log("eee"+result);
+            });
+            StartCoroutine(dissmiss(id, 3f));
+        }
+    }
+    
+    IEnumerator dissmiss (int id, float time)
+    {
+        yield return new WaitForSeconds (time);
+        DialogManager.Instance.DissmissDialog (id);
+    }
+}
